095106d51b7eb2d2e7a5ecceea3cfd4d125616d0
zzawang <hobin0116@naver.com>
2024-03-11 13:38:43 +0900

    feat: 정적인 html 파일 응답 기능 구현
    - 라인별로 http header를 읽고, 적절하게 파싱해서 로거를 이용해서 출력한다.
    - 파일의 내용을 바이트 배열로 변환해주는 메소드를 따로 구현하여 사용하였다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,15 +1,25 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 
+import java.nio.file.Files;
+import java.util.regex.Pattern;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestHandler implements Runnable {
+    private static final String BLANK = "\s+";
+    private static final String BASE_PATH = "./src/main/resources/static";
+    private static final int URL_INDEX = 1;
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
@@ -23,13 +33,33 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String filePath = br.readLine();
+
+            logger.debug("request line : {}", filePath);
+            String url = filePath.split(BLANK)[URL_INDEX];
+            byte[] body = readAllBytes(BASE_PATH + url);
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "<h1>Hello World</h1>".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
+        }
+    }
+
+    private byte[] readAllBytes(String filePath) throws IOException {
+        File file = new File(filePath);
+        try (FileInputStream inputStream = new FileInputStream(file)) {
+            byte[] buffer = new byte[(int) file.length()];
+            int bytesRead = inputStream.read(buffer); // 파일 내용 읽기
+
+            if (bytesRead != file.length()) {
+                throw new IOException();
+            }
+            return buffer;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
