5f49f1c54592e1e3f6e22afd97d3b964a33d7489
zzawang <hobin0116@naver.com>
2024-03-12 11:05:35 +0900

    tes: UserTest에 User 생성과 toString()메소드 테스트 추가
    - User가 올바른 아이디, 비밀번호, 이름, 이메일 값으로 생성될 수 있는지 검증한다.
    - User의 toString()메소드가 알맞은 문자열로 생성되는지 검증한다.

---
+++ src/test/java/model/UserTest.java
@@ -0,0 +1,31 @@
+package model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+    private static final String ID = "hobinchoi";
+    private static final String PASSWORD = "codesquad";
+    private static final String NAME = "최호빈";
+    private static final String EMAIL = "hobin0116@naver.com";
+
+    @DisplayName("User가 올바른 아이디, 비밀번호, 이름, 이메일 값으로 생성될 수 있다.")
+    @Test
+    void create() {
+        User user = new User(ID, PASSWORD, NAME, EMAIL);
+        assertThat(user.getUserId()).isEqualTo(ID);
+        assertThat(user.getPassword()).isEqualTo(PASSWORD);
+        assertThat(user.getName()).isEqualTo(NAME);
+        assertThat(user.getEmail()).isEqualTo(EMAIL);
+    }
+
+    @DisplayName("User의 toString()메소드가 알맞은 문자열로 생성될 수 있다.")
+    @Test
+    void userToString() {
+        User user = new User(ID, PASSWORD, NAME, EMAIL);
+        String result = "User [userId=hobinchoi, password=codesquad, name=최호빈, email=hobin0116@naver.com]";
+        assertThat(user.toString()).isEqualTo(result);
+    }
+}
