332f0a5877954ac96597b8e29cf0dc499960cd1a
zzawang <hobin0116@naver.com>
2024-03-15 02:14:19 +0900

    test: ParameterizedTest를 사용하도록 수정
    - 문자열 상수를 가져와 검증하는 것이 아니라, CsvSource와 ValueSource를 사용하여 ParameterizedTest를 할 수 있도록 수정하였다.

--- src/test/java/utils/ExtractorTest.java
+++ src/test/java/utils/ExtractorTest.java
@@ -6,11 +6,16 @@ import java.io.File;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ExtractorTest {
     private static final String BLANK = "\s+";
-    private static final List<String> HTTP_REQUEST_HEADER = List.of(
-            "GET /index.html HTTP/1.1",
+
+    @DisplayName("공백을 기준으로 문자열을 추출할 수 있다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"GET /index.html HTTP/1.1",
             "GET /reset.css HTTP/1.1",
             "GET /global.css HTTP/1.1",
             "GET /main.css HTTP/1.1",
@@ -20,43 +25,47 @@ class ExtractorTest {
             "GET /img/bookMark.svg HTTP/1.1",
             "GET /img/ci_chevron-left.svg HTTP/1.1",
             "GET /img/ci_chevron-right.svg HTTP/1.1",
-            "GET /favicon.ico HTTP/1.1);");
-    private static final List<String> FILE_PATH = List.of(
-           "/index.html",
-           "/reset.css",
-            "/global.css",
-           "/main.css",
-           "/img/signiture.svg",
-            "/img/like.svg",
-            "/img/sendLink.svg",
-            "/img/bookMark.svg",
-            "/img/ci_chevron-left.svg",
-            "/img/ci_chevron-right.svg",
-            "/favicon.ico");
-
-    @DisplayName("공백을 기준으로 문자열을 추출할 수 있다.")
-    @Test
-    void extract() {
-        HTTP_REQUEST_HEADER.forEach(header -> assertThat(Extractor.extract(header)).isEqualTo(header.split(BLANK)));
+            "GET /favicon.ico HTTP/1.1)"
+    })
+    void extract(String httpHeader) {
+        assertThat(Extractor.extract(httpHeader)).isEqualTo(httpHeader.split(BLANK));
     }
 
-    @DisplayName("Http Request에서 url을 추출할 수 있다.")
-    @Test
-    void extractUrl() {
-        for (int index = 0; index < HTTP_REQUEST_HEADER.size(); index++) {
-            String url = Extractor.extractUrl(HTTP_REQUEST_HEADER.get(index));
-            assertThat(url).isEqualTo(FILE_PATH.get(index));
-            String filePath = DirectoryMatcher.mathDirectory(url);
-            File file = new File(filePath);
-            assertThat(file.isFile()).isTrue();
-            assertThat(file.length()).isGreaterThan(0);
-        }
+    @DisplayName("Http Request Header에서 url을 추출할 수 있다.")
+    @ParameterizedTest
+    @CsvSource({
+            "GET /index.html HTTP/1.1, /index.html",
+            "GET /reset.css HTTP/1.1, /reset.css",
+            "GET /global.css HTTP/1.1, /global.css",
+            "GET /main.css HTTP/1.1, /main.css",
+            "GET /img/signiture.svg HTTP/1.1, /img/signiture.svg",
+            "GET /img/like.svg HTTP/1.1, /img/like.svg",
+            "GET /img/sendLink.svg HTTP/1.1, /img/sendLink.svg",
+            "GET /img/bookMark.svg HTTP/1.1, /img/bookMark.svg",
+            "GET /img/ci_chevron-left.svg HTTP/1.1, /img/ci_chevron-left.svg",
+            "GET /img/ci_chevron-right.svg HTTP/1.1, /img/ci_chevron-right.svg",
+            "GET /favicon.ico HTTP/1.1, /favicon.ico"
+    })
+    void extractUrl(String httpHeader, String url) {
+        assertThat(Extractor.extractUrl(httpHeader)).isEqualTo(url);
     }
 
     @DisplayName("Http Request에서 Http Method를 추출할 수 있다.")
-    @Test
-    void extractMethod() {
-        HTTP_REQUEST_HEADER.forEach(header -> assertThat(Extractor.extractMethod(header)).isEqualTo("GET"));
+    @ParameterizedTest
+    @ValueSource(strings = {"GET /index.html HTTP/1.1",
+            "GET /reset.css HTTP/1.1",
+            "GET /global.css HTTP/1.1",
+            "GET /main.css HTTP/1.1",
+            "GET /img/signiture.svg HTTP/1.1",
+            "GET /img/like.svg HTTP/1.1",
+            "GET /img/sendLink.svg HTTP/1.1",
+            "GET /img/bookMark.svg HTTP/1.1",
+            "GET /img/ci_chevron-left.svg HTTP/1.1",
+            "GET /img/ci_chevron-right.svg HTTP/1.1",
+            "GET /favicon.ico HTTP/1.1)"
+    })
+    void extractMethod(String httpHeader) {
+        assertThat(Extractor.extractMethod(httpHeader)).isEqualTo("GET");
     }
 
     @DisplayName("user 생성 요청에서 user의 아이디, 비밀번호, 이름, 이메일을 추출할 수 있다.")
