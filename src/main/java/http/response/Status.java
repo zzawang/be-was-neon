a6b999d2726aa1b592eeca0d7ca5765d600792fc
zzawang <hobin0116@naver.com>
2024-03-19 10:55:55 +0900

    feat: Status에 HTTP 상태 코드 추가
    - 웹 개발에서 자주 사용되는 HTTP 상태 코드 중 일부를 추가하였다.

--- src/main/java/http/response/Status.java
+++ src/main/java/http/response/Status.java
@@ -2,8 +2,17 @@ package http.response;
 
 public enum Status {
     OK("200", "OK"),
+    CREATED("201", "Created"),
+    NO_CONTENT("204", "No Content"),
+    REDIRECT("302", "Found"),
+    NOT_MODIFIED("304", "Not Modified"),
     BAD_REQUEST("400", "Bad Request"),
-    NOT_FOUND("404", "Not Found");
+    UNAUTHORIZED("401", "Unauthorized"),
+    FORBIDDEN("403", "Forbidden"),
+    NOT_FOUND("404", "Not Found"),
+    METHOD_NOT_ALLOWED("405", "Method Not Allowed"),
+    INTERNAL_SERVER_ERROR("500", "Internal Server Error"),
+    SERVICE_UNAVAILABLE("503", "Service Unavailable");
 
     private final String code;
     private final String msg;
