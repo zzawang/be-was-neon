822a364da736ad09674ed27203943449ebf1a8de
zzawang <hobin0116@naver.com>
2024-03-19 11:19:58 +0900

    feat: HttpResponse에 리다이렉트하는 응답 기능 추가
    - status를 REDIRECT로 설정하고 location 값을 기본 경로로 지정한다.

--- src/main/java/http/response/HttpResponse.java
+++ src/main/java/http/response/HttpResponse.java
@@ -1,5 +1,9 @@
 package http.response;
 
+import static utils.Constant.BASIC_ROUTE;
+import static utils.Constant.CRLF;
+import static utils.Constant.ERROR_MSG_FORMAT;
+
 import http.Version;
 import http.Version.ProtocolVersion;
 
@@ -8,6 +12,7 @@ public class HttpResponse {
     private Status status;
     private Integer contentLength;
     private ContentType contentType;
+    private String location;
     private byte[] httpRequestBody;
 
     public void setOkResponse(ContentType contentType, byte[] httpRequestBody) {
@@ -18,8 +23,14 @@ public class HttpResponse {
         this.httpRequestBody = httpRequestBody;
     }
 
+    public void setRedirectResponse() {
+        this.version = new Version(ProtocolVersion.V_11.getVersion());
+        this.status = Status.REDIRECT;
+        this.location = BASIC_ROUTE;
+    }
+
     public void setErrorResponse(Status status) {
-        byte[] httpRequestBody = String.format("<h1>%s</h1>", status.getMsg()).getBytes();
+        byte[] httpRequestBody = String.format(ERROR_MSG_FORMAT, status.getMsg()).getBytes();
         this.version = new Version(ProtocolVersion.V_11.getVersion());
         this.status = status;
         this.contentLength = httpRequestBody.length;
@@ -27,19 +38,19 @@ public class HttpResponse {
         this.httpRequestBody = httpRequestBody;
     }
 
-    public String getFormattedFirstLine() {
-        return String.format("%s %s %s", version, status.getCode(), status.getMsg());
-    }
-
-    public String getFormattedContentType() {
-        return String.format("Content-Type: %s", contentType.getContentType());
-    }
-
-    public String getFormattedContentLength() {
-        return String.format("Content-Length: %s", contentLength.intValue());
-    }
-
     public byte[] getHttpRequestBody() {
         return httpRequestBody;
+    }
+
+    public String getFormattedResponse() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("%s %s %s", version.getVersion(), status.getCode(), status.getMsg())).append(CRLF);
+        if (status.equals(Status.REDIRECT)) {
+            sb.append(String.format("Location: %s", location)).append(CRLF);
+            return sb.toString();
+        }
+        sb.append(String.format("Content-Type: %s", contentType.getContentType())).append(CRLF);
+        sb.append(String.format("Content-Length: %s", contentLength)).append(CRLF);
+        return sb.toString();
     }
 }
