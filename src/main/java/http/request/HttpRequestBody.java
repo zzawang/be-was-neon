52403efe52d4a7ff3726c1d9720d44fc4d145b55
zzawang <hobin0116@naver.com>
2024-03-19 11:11:17 +0900

    feat: HttpRequestBody에 유저 정보를 추출하는 기능 추가
    - body에서 유저의 id, pw, name, email만 추출한다.

--- src/main/java/http/request/HttpRequestBody.java
+++ src/main/java/http/request/HttpRequestBody.java
@@ -1,9 +1,20 @@
 package http.request;
 
+import static utils.Constant.EMPTY;
+
 public class HttpRequestBody {
+    private static final String REPLACE_ID = "userId|userPw|userName|userEmail|=";
+    private static final String SPLIT_REGEX = "&";
+
     private final String body;
 
     public HttpRequestBody(String body) {
         this.body = body;
+    }
+
+
+    public String[] extractUser() {
+        String bodyValue = body.replaceAll(REPLACE_ID, EMPTY);
+        return bodyValue.split(SPLIT_REGEX); // id, pw, name, email만 추출
     }
 }
