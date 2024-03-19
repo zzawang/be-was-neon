cf3423be357b382a866f553c94ff957f7629b807
zzawang <hobin0116@naver.com>
2024-03-19 10:56:48 +0900

    fix: 매핑 기능 수정
    - 기본 경로인 "/"으로 접근하여도 메인 화면이 보이도록 추가하였다.
    - 로그인 페이지, 회원가입 페이지 같이 폴더 안에 들어있는 페이지들을 폴더명으로 입력해도 보이도록 매핑 정보를 추가하였다.

--- src/main/java/utils/DirectoryMatcher.java
+++ src/main/java/utils/DirectoryMatcher.java
@@ -2,6 +2,8 @@ package utils;
 
 public class DirectoryMatcher {
     private static final String BASE_PATH = "src/main/resources/static";
+    private static final String BASIC_ROUTE = "/";
+    private static final String BASIC_PATH = "/index.html";
     private static final String ARTICLE_PATH = "/article";
     private static final String ARTICLE_HTML = "/article.html";
     private static final String COMMENT_PATH = "/comment";
@@ -12,18 +14,21 @@ public class DirectoryMatcher {
     private static final String LOGIN_HTML = "/login.html";
 
     public static String mathDirectory(String filePath) {
+        if (filePath.equals(BASIC_ROUTE) || filePath.equals(BASIC_PATH)) {
+            return BASE_PATH + BASIC_PATH;
+        }
         if (filePath.equals(ARTICLE_PATH) || filePath.equals(ARTICLE_HTML)) {
-            return BASE_PATH + ARTICLE_PATH + filePath;
+            return BASE_PATH + ARTICLE_PATH + ARTICLE_HTML;
         }
         if (filePath.equals(COMMENT_PATH) || filePath.equals(COMMENT_HTML)) {
-            return BASE_PATH + COMMENT_PATH + filePath;
+            return BASE_PATH + COMMENT_PATH + COMMENT_HTML;
         }
         if (filePath.equals(REGISTRATION_PATH) || filePath.equals(REGISTRATION_HTML)) {
-            return BASE_PATH + REGISTRATION_PATH + filePath;
+            return BASE_PATH + REGISTRATION_PATH + REGISTRATION_HTML;
         }
         if (filePath.equals(LOGIN_PATH) || filePath.equals(LOGIN_HTML)) {
-            return BASE_PATH + LOGIN_PATH + filePath;
+            return BASE_PATH + LOGIN_PATH + LOGIN_HTML;
         }
-        return BASE_PATH + filePath; // 폴더 안에 생성된 파일이 아닌 경우
+        return BASE_PATH + filePath; // 나머지는 기본 경로
     }
 }
