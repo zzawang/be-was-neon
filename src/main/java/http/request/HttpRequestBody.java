7b4f45ee2e748c4a12e24e7d4469bbb10763b7ce
zzawang <hobin0116@naver.com>
2024-03-19 16:47:46 +0900

    fix: 유저 추출 기능에 기본 값 설정
    - 로그인 기능과 회원가입 기능에 이 메소드가 사용되기 위해서는 이름과 이메일 변수에 기본값이 들어가야 하므로 추출할때 공백으로 기본값을 설정해준다.

--- src/main/java/http/request/HttpRequestBody.java
+++ src/main/java/http/request/HttpRequestBody.java
@@ -15,6 +15,11 @@ public class HttpRequestBody {
 
     public String[] extractUser() {
         String bodyValue = body.replaceAll(REPLACE_ID, EMPTY);
-        return bodyValue.split(SPLIT_REGEX); // id, pw, name, email만 추출
+        String[] result = new String[]{EMPTY, EMPTY, EMPTY, EMPTY};
+        String[] values = bodyValue.split(SPLIT_REGEX); // id, pw, name, email만 추출
+
+        // 분할된 값들을 배열에 넣기
+        System.arraycopy(values, 0, result, 0, values.length);
+        return result;
     }
 }
