c05a7e4f7b5340c2ba542797d68e2bc30fc700fe
zzawang <hobin0116@naver.com>
2024-03-19 13:20:52 +0900

    feat: ResponseHeader를 클래스로 분리
    - ResponseHeader를 클래스로 분리하여 contentType, contentLength 같은 헤더 값들을 인스턴스 변수로 가지도록 하였다.

--- src/main/java/http/response/HttpResponse.java
+++ src/main/java/http/response/HttpResponse.java
@@ -10,31 +10,27 @@ import http.Version.ProtocolVersion;
 public class HttpResponse {
     private Version version;
     private Status status;
-    private Integer contentLength;
-    private ContentType contentType;
-    private String location;
+    private ResponseHeaders responseHeaders;
     private byte[] httpRequestBody;
 
     public void setOkResponse(ContentType contentType, byte[] httpRequestBody) {
         this.version = new Version(ProtocolVersion.V_11.getVersion());
         this.status = Status.OK;
-        this.contentLength = httpRequestBody.length;
-        this.contentType = contentType;
+        responseHeaders = new ResponseHeaders(httpRequestBody.length, contentType);
         this.httpRequestBody = httpRequestBody;
     }
 
     public void setRedirectResponse() {
         this.version = new Version(ProtocolVersion.V_11.getVersion());
         this.status = Status.REDIRECT;
-        this.location = BASIC_ROUTE;
+        responseHeaders = new ResponseHeaders(BASIC_ROUTE);
     }
 
     public void setErrorResponse(Status status) {
         byte[] httpRequestBody = String.format(ERROR_MSG_FORMAT, status.getMsg()).getBytes();
         this.version = new Version(ProtocolVersion.V_11.getVersion());
         this.status = status;
-        this.contentLength = httpRequestBody.length;
-        this.contentType = ContentType.html;
+        responseHeaders = new ResponseHeaders(httpRequestBody.length, ContentType.html);
         this.httpRequestBody = httpRequestBody;
     }
 
@@ -46,11 +42,11 @@ public class HttpResponse {
         StringBuilder sb = new StringBuilder();
         sb.append(String.format("%s %s %s", version.getVersion(), status.getCode(), status.getMsg())).append(CRLF);
         if (status.equals(Status.REDIRECT)) {
-            sb.append(String.format("Location: %s", location)).append(CRLF);
+            sb.append(String.format("Location: %s", responseHeaders.getLocation())).append(CRLF);
             return sb.toString();
         }
-        sb.append(String.format("Content-Type: %s", contentType.getContentType())).append(CRLF);
-        sb.append(String.format("Content-Length: %s", contentLength)).append(CRLF);
+        sb.append(String.format("Content-Type: %s", responseHeaders.getContentType())).append(CRLF);
+        sb.append(String.format("Content-Length: %s", responseHeaders.getContentLength())).append(CRLF);
         return sb.toString();
     }
 }
