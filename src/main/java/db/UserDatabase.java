36cfb0adc3bef67d429bc6ad7d821e02af8bd6ad
zzawang <hobin0116@naver.com>
2024-03-28 16:45:40 +0900

    feat: 유저 저장하는 키값 변경
    - user의 name이 중복될 수 있기 때문에 키값을 id로 변경하였다.

--- src/main/java/db/UserDatabase.java
+++ src/main/java/db/UserDatabase.java
@@ -9,18 +9,18 @@ public class UserDatabase {
     private static Map<String, User> users = new ConcurrentHashMap<>();
 
     public static void addUser(User user) {
-        users.put(user.getName(), user);
-    }
-
-    public static User findUserById(String userId) {
-        return users.values().stream()
-                .filter(user -> user.getId().equals(userId))
-                .findFirst()
-                .orElse(null); // 해당 ID를 가진 사용자를 찾을 수 없을 경우 null 반환
+        users.put(user.getId(), user);
     }
 
     public static User findUserByName(String userName) {
-        return users.get(userName);
+        return users.values().stream()
+                .filter(user -> user.getName().equals(userName))
+                .findFirst()
+                .orElse(null); // 해당 name을 가진 사용자를 찾을 수 없을 경우 null 반환
+    }
+
+    public static User findUserById(String userId) {
+        return users.get(userId);
     }
 
     public static Collection<User> findAll() {
